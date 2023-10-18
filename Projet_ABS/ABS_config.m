e954bc843424b41b799866fc0d38541de995b591
YoAB25 <youssef.learn.ab.25@gmail.com>
2023-10-18 22:08:46 +0200

    Implemented Final Version : ABS + EBD system

--- Projet_ABS/ABS_config.m
+++ Projet_ABS/ABS_config.m
@@ -1,6 +1,7 @@
 %% Projet : Modélisation du freinage d'une automobile ABS
 
 % Paramètres du système
+g = 9.81;
 g0 = 9.81;
 v0 = 27.77;
 r_Roue = 0.3;
@@ -10,6 +11,7 @@ m = 1000;
 Omega_0 = v0/r_Roue;
 
 % Paramètre de la partie 2 : EBD
+e = 3;
 d = 1.5;
 D = 3;
 h = 0.7;
@@ -17,3 +19,4 @@ h = 0.7;
 V0 = 27.77;
 rR = 0.3;
 w0 = V0/rR;
+gp = 9.81;
